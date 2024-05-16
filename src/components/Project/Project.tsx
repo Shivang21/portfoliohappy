6096c34b16caa32459fd80ed3fd6de28cafcf117
codewrapperz2021 <codewrapperz360@gmail.com>
2024-05-16 23:28:27 +0530

    hvhjv

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -152,21 +152,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://velocityai.vinayaksingh.com/" target="_blank" rel="noreferrer">
+                <a href="https://weatherbysg.netlify.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>VeloCityAI</h3>
+              <h3>Weather Web App</h3>
               <p>
-                VelocityAI is a website that allows users to get instant answers to any question and generate stunning AI pictures using OpenAI's GPT-3 and DALL-E model.With VelocityAI, users can type in text-based prompts and receive a corresponding response.
-              </p>
+              This project encompasses a comprehensive weather web application built with HTML, CSS, and JavaScript, augmented with RESTful APIs. It's engineered to fetch and display real-time weather data for multiple cities, enhancing its utility and scope. The user-friendly interface allows seamless navigation and exploration of weather conditions across various locations. With intuitive design and robust functionality, users can effortlessly check the weather in different cities, empowering informed decision-making.</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>React</li>
-                <li>Typescript</li>
+                <li>Javascript</li>
+                <li>RestApi</li>
                 <li>Html</li>
                 <li>css</li>
               </ul>
